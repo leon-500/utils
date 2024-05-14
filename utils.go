e8c555ee606ed32f55caf351467798f69ffdce0c
leon-500 <126183173+leon-500@users.noreply.github.com>
2024-05-14 22:58:20 +0500

    Update utils.go
    func InSlice

--- utils.go
+++ utils.go
@@ -1,12 +1,12 @@
 package utils
 
-func Contains(a []string, x string) bool {
-	for _, n := range a {
-		if x == n {
-			return true
-		}
-	}
-	return false
+func InSlice(a []string, x string) bool {
+  for _, n := range a {  
+     if x == n {     
+        return true     
+     }  
+  }  
+  return false
 }
 
 func ContainsInt(a []int, x int) bool {
